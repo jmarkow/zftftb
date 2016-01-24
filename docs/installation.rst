d32fa9bd8e244ea81233ba8f05352cd7ac4931b8
Jeff Markowitz <markowitzmeister@gmail.com>
2016-01-24 17:02:44 -0500

    Added install script and updated docs

--- docs/installation.rst
+++ docs/installation.rst
@@ -6,7 +6,24 @@ Simply clone the ZFTFTB github repository, and add the directory and subdirector
 Requirements
 ------------
 
-This has been tested using MATLAB 2010A and later on Windows and Mac (Linux should be fine). You must have the `markolab <https://github.com/jmarkow/markolab/>`_ toolbox in your MATLAB path. The only MATLAB Toolbox required is the Signal Processing toolbox, which is typically included in most standard installations.
+This has been tested using MATLAB 2010A and later on Windows and Mac (Linux should be fine). You must have the `markolab <https://github.com/jmarkow/markolab/>`_ toolbox in your MATLAB path. The only MATLAB Toolbox required is the Signal Processing toolbox, which is typically included in most standard installations.  It is highly recommended that you have git installed for ease of installation and managing dependencies.
 
 Using the install script
 ------------------------
+
+If you are running MATLAB on Linux or OS X, a script is available to automatically add ZFTFTB and any necessary dependencies to your MATLAB path.  Then in MATLAB navigate to the repository or unzipped directory of files::
+
+  >>cd ~/Downloads/ZFTFTB.git/
+  >>zftftb_install
+
+You should be prompted to select a base directory for dependencies (e.g. :code:`~/Documents/MATLAB`).  Then, assuming git is installed and your pathdef.m is writable, the rest should be taken care of for you.
+
+Manual installation
+-------------------
+
+If you are somewhat comfortable with the command line and MATLAB, manual installation shouldn't be too onerous.  If you are working with OS X or Linux, pop open a terminal and clone the ZFTFTB and Markolab repositories::
+
+  $git clone git@github.com:jmarkow/zftftb.git
+  $git clone git@github.com:jmarkow/markolab.git
+
+Then, make sure the repositories and their sub-directories are added to the MATLAB path.  
---
+++ zftftb_install.m
@@ -0,0 +1,110 @@
+function zftftb_install()
+% run this script to add appropriate directories to the MATLAB path, also handles
+% dependencies
+%
+
+% where are we?
+
+cur_file=mfilename('fullpath');
+[cur_path,~,~]=fileparts(cur_file);
+
+% name of package
+
+package_name='zftftb';
+
+% dependencies
+
+dependency(1).name='markolab';
+dependency(1).chk='markolab_multi_fig_save';
+dependency(1).url='git@github.com:jmarkow/markolab.git';
+dependency(1).basedir='';
+
+dependency(2).name='zftftb';
+dependency(2).chk='';
+dependency(2).url='';
+dependency(2).basedir=cur_path;
+
+% dirs to skip
+
+skip_dirs={'.git','docs'};
+
+if ~isunix
+  error('Install script only works on OS X or Linux.');
+end
+
+% check for git
+
+status=unix('git version');
+
+if status~=0
+  error('Git must be installed before continuing.');
+end
+
+% check for markolab dependency
+
+fprintf('Checking dependencies...\n')
+
+matlab_path=regexp(path,'\:','split');
+package_added=strfind(matlab_path,cur_path);
+idx=cellfun(@(x) ~isempty(x),package_added);
+package_added=any(idx);
+
+for i=1:length(dependency)
+
+  package_flag=strcmp(dependency(i).name,package_name);
+
+  if (package_flag&~package_added) | ...
+    (~isempty(dependency(i).chk)&isempty(which(dependency(i).chk)))
+
+    if isempty(dependency(i).basedir)
+      tmp=uigetdir(pwd,'Select directory to place repository in');
+      dependency(i).basedir=tmp;
+    end
+
+    if ~exist(fullfile(dependency(i).basedir,dependency(i).name),'dir') & ~package_flag
+      if ~isempty(dependency(i).url)
+        status=unix(['git clone ' dependency(i).url ' ' ...
+        fullfile(dependency(i).basedir,dependency(i).name)]);
+      end
+    else
+      fprintf('Directory already exists, adding to MATLAB path...\n')
+    end
+
+    if status~=0
+      error('Error installing dependency %s',dependency(i).name);
+    end
+
+    if package_flag
+      tmp=genpath(cur_path);
+    else
+      tmp=genpath(fullfile(dependency(i).basedir,dependency(i).name));
+    end
+
+    splits=regexp(tmp,'\:','split');
+
+    for j=1:length(skip_dirs)
+      idx=cellfun(@(x) ~isempty(x),strfind(splits,skip_dirs{j}));
+      splits(idx)=[];
+    end
+
+    emptyidx=cellfun(@isempty,splits);
+    splits(emptyidx)=[];
+
+    newstr='';
+
+    for j=1:length(splits)
+      newstr=[newstr splits{j} ':'];
+    end
+
+    addpath(newstr);
+    status=savepath;
+
+    if status~=0
+      error('Error saving new MATLAB path, make sure pathdef.m is writable');
+    end
+
+    fprintf('%s installed successfully\n',dependency(i).name);
+  else
+    fprintf('Dependency %s already installed\n',dependency(i).name)
+  end
+end
